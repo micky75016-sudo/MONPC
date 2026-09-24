63229250fb940ff90702700cd4487e122de67a96
sormickPC <micky75016@gmail.com>
2026-09-24 10:45:13 +0200

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,300 @@
+window.WEBOS_CONFIG = {
+  "grid": {
+    "ox": 24,
+    "oy": 55,
+    "uw": 120,
+    "uh": 124
+  },
+  "desktopIcons": [
+    {
+      "key": "icon--1790215512039",
+      "label": "MES CLOUDS",
+      "url": "file:///C:/Users/micky/Downloads/TEST/index.html?webos-folder=1",
+      "favicon": "",
+      "col": 0,
+      "row": 0
+    },
+    {
+      "key": "icon-app-transferchain-io-1790215522158",
+      "label": "Transferchain",
+      "url": "https://app.transferchain.io/pass/passwords",
+      "favicon": "app.transferchain.io",
+      "col": 0,
+      "row": 1
+    },
+    {
+      "key": "icon-sormicky-thesecurenote-com-1790219698585",
+      "label": "Secure Notes",
+      "url": "https://sormicky.thesecurenote.com/",
+      "favicon": "sormicky.thesecurenote.com",
+      "col": 1,
+      "row": 0
+    },
+    {
+      "key": "icon-google-fr-1790220035410",
+      "label": "Google",
+      "url": "https://www.google.fr/",
+      "favicon": "google.fr",
+      "col": 2,
+      "row": 0
+    },
+    {
+      "key": "icon-youtube-com-1790220145716",
+      "label": "Youtube",
+      "url": "https://www.youtube.com/",
+      "favicon": "youtube.com",
+      "col": 1,
+      "row": 1
+    }
+  ],
+  "dock": [
+    "separator",
+    {
+      "label": "Navigateur Web",
+      "url": "https://browser.lol/en/viewer",
+      "favicon": "browser.lol"
+    },
+    {
+      "label": "Voila AI",
+      "url": "https://www.getvoila.ai/chat",
+      "favicon": "getvoila.ai"
+    },
+    {
+      "label": "Okara AI",
+      "url": "https://okara.ai/chat",
+      "favicon": "okara.ai"
+    },
+    {
+      "label": "Google AI",
+      "url": "https://google.com/ai",
+      "favicon": "google.com"
+    },
+    {
+      "label": "Transferchain",
+      "url": "https://app.transferchain.io/pass/passwords",
+      "favicon": "app.transferchain.io"
+    },
+    {
+      "label": "Pcloud",
+      "url": "https://pcloud.com/",
+      "favicon": "pcloud.com"
+    },
+    {
+      "label": "Internxt",
+      "url": "https://drive.internxt.com/app",
+      "favicon": "drive.internxt.com"
+    }
+  ],
+  "bookmarkSections": [
+    {
+      "blocks": [
+        {
+          "title": "APPRENTISSAGE",
+          "links": [
+            {
+              "label": "Voila AI",
+              "url": "https://www.getvoila.ai/chat"
+            },
+            {
+              "label": "Okara AI",
+              "url": "https://okara.ai/chat"
+            },
+            {
+              "label": "Google AI",
+              "url": "https://google.com/ai"
+            },
+            {
+              "label": "Youtube",
+              "url": "https://www.youtube.com"
+            }
+          ]
+        },
+        {
+          "title": "MES CLOUDS",
+          "links": [
+            {
+              "label": "Transferchain",
+              "url": "https://app.transferchain.io/pass/passwords"
+            },
+            {
+              "label": "Pcloud",
+              "url": "https://pcloud.com"
+            },
+            {
+              "label": "Internxt",
+              "url": "https://drive.internxt.com/app"
+            },
+            {
+              "label": "Deskfirst",
+              "url": "https://app.deskfirst.com/auth/profile"
+            },
+            {
+              "label": "Icedrive",
+              "url": "https://icedrive.net/dashboard/#/cloud"
+            },
+            {
+              "label": "Degoo",
+              "url": "https://app.degoo.com/files/0"
+            },
+            {
+              "label": "Koofr",
+              "url": "https://app.koofr.net/app/storage/3e3131e3-623d-4ffd-be7b-d28c740ebc2c"
+            },
+            {
+              "label": "Files FM",
+              "url": "https://files.fm/filebrowser#/"
+            },
+            {
+              "label": "Offcloud",
+              "url": "https://offcloud.com/#/instant"
+            },
+            {
+              "label": "Starchive",
+              "url": "https://app.starchive.io"
+            },
+            {
+              "label": "Cozy IO",
+              "url": "https://sormick-home.mycozy.cloud/#/connected"
+            },
+            {
+              "label": "Cube",
+              "url": "https://cube.securibox.eu/_a/dashboard"
+            },
+            {
+              "label": "Transfercloud",
+              "url": "https://transfercloud.io/"
+            },
+            {
+              "label": "Lifecloud",
+              "url": "https://applifecloud.com/account"
+            },
+            {
+              "label": "Multcloud",
+              "url": "https://www.multcloud.com"
+            },
+            {
+              "label": "Burst",
+              "url": "https://www.burst.com"
+            },
+            {
+              "label": "Filen",
+              "url": "https://filen.io/my-account/file-manager"
+            },
+            {
+              "label": "Amaryllo Cloud",
+              "url": "https://acs.amaryllo.us/home"
+            },
+            {
+              "label": "SFTP CLOUD",
+              "url": "https://app.sftpcloud.io"
+            },
+            {
+              "label": "Filestreams",
+              "url": "https://filestreams.com/account"
+            },
+            {
+              "label": "Blaze",
+              "url": "https://app.blaze.cx"
+            },
+            {
+              "label": "Prismdrive",
+              "url": "https://app.prismdrive.com/drive"
+            },
+            {
+              "label": "Cubbit",
+              "url": "https://web.cubbit.io/home/manager/"
+            },
+            {
+              "label": "Mobidrive",
+              "url": "https://www.mobidrive.com/my-files"
+            },
+            {
+              "label": "ABIGGB",
+              "url": "https://abiggb.com"
+            },
+            {
+              "label": "Puter",
+              "url": "https://puter.com"
+            },
+            {
+              "label": "Getscreen ME",
+              "url": "https://getscreen.me/dashboard/agents"
+            },
+            {
+              "label": "Voxfor",
+              "url": "https://my.voxfor.com/clientarea.php"
+            },
+            {
+              "label": "Filelu",
+              "url": "https://filelu.com"
+            },
+            {
+              "label": "DWService",
+              "url": "https://access.dwservice.net/login.dw?localeid=fr#s"
+            },
+            {
+              "label": "Jiocloud",
+              "url": "https://www.jiocloud.com"
+            },
+            {
+              "label": "VPS",
+              "url": "https://go.getscreen.me/lub-zcq-wbj"
+            },
+            {
+              "label": "scamble  Cloud",
+              "url": "https://scramble.cloud/#drive"
+            },
+            {
+              "label": "Sava OS",
+              "url": "https://savaos.com"
+            },
+            {
+              "label": "Drime Cloud",
+              "url": "https://app.drime.cloud/drive"
+            },
+            {
+              "label": "Felicloud",
+              "url": "https://cloud.felicloud.com/apps/files"
+            },
+            {
+              "label": "JumDesktop",
+              "url": "https://app.jumpdesktop.com"
+            }
+          ]
+        },
+        {
+          "title": "Documents-Taches",
+          "links": [
+            {
+              "label": "Secure Notes",
+              "url": "https://sormicky.thesecurenote.com"
+            }
+          ]
+        },
+        {
+          "title": "Recherche Web",
+          "links": [
+            {
+              "label": "Google",
+              "url": "https://www.google.fr/"
+            }
+          ]
+        },
+        {
+          "title": "TV MONDE",
+          "links": []
+        },
+        {
+          "title": "OUTILS",
+          "links": [
+            {
+              "label": "Navigateur Web",
+              "url": "https://browser.lol/en/viewer"
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "customWallpaper": "https://getwallpapers.com/wallpaper/full/c/5/3/855051-gorgerous-beautiful-desktop-background-1920x1080.jpg"
+};
